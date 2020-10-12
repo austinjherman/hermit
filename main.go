49bbaa88e513dce3071c969d0a18e6ff94ae8bd5
Austin Herman <austin.j.herman@gmail.com>
2020-10-12 19:25:29 -0400

    add rand function. rename to_upper to caps.

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"flag"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	
-	toUpperCmd := flag.NewFlagSet("to_upper", flag.ExitOnError)
+	toCapsCmd := flag.NewFlagSet("caps", flag.ExitOnError)
+	randCmd := flag.NewFlagSet("rand", flag.ExitOnError)
+	randMax := randCmd.Int("mx", 100, "the maximum")
+	randMin := randCmd.Int("mn", 0, "the minimun")
 
-	if len(os.Args) < 3 {
+	if len(os.Args) < 2 {
 		fmt.Println("usage: hermit <command> [<args>]")
 		fmt.Printf("given: %s\n", os.Args)
 		os.Exit(1)
@@ -19,11 +24,17 @@ func main() {
 
 	switch os.Args[1] {
 
-		case "to_upper":
-			toUpperCmd.Parse(os.Args[2:])
-			args := toUpperCmd.Args()
+		case "caps":
+			toCapsCmd.Parse(os.Args[2:])
+			args := toCapsCmd.Args()
 			argsString := strings.Join(args, " ")
 			fmt.Println(strings.ToUpper(argsString))
+			os.Exit(0)
+
+		case "rand":
+			randCmd.Parse(os.Args[2:])
+			rand.Seed(time.Now().UnixNano())
+			fmt.Println(rand.Intn(*randMax + 1 - *randMin) + *randMin)
 			os.Exit(0)
 
 		default:
